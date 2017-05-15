41f130a01ad1a0b9fc748f9ad65281eeb1c7d822
Richard Pablo <richardpablop@gmail.com>
2017-05-16 00:00:02 +0100

    Fixing heroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero.model';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Angus Young' },
+  { id: 12, name: 'Pele' },
+  { id: 13, name: 'Jimmy Hendrix' },
+  { id: 14, name: 'Neruda' },
+  { id: 15, name: 'Turin' },
+  { id: 16, name: 'Chaplin' },
+  { id: 17, name: 'Linus' },
+  { id: 18, name: 'Hawking' },
+  { id: 19, name: 'Einstein' },
+  { id: 20, name: 'Mandela' }
 ];
